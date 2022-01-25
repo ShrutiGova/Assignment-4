5209aacf290c38dd30899d29003d8d27fb93fbc1
ShrutiGova <shrutigova@gmail.com>
2022-01-25 18:12:39 +0530

    Initial commit

---
+++ Assignment4.js
@@ -0,0 +1,65 @@
+//Assignment 4
+
+//Operators in js
+
+//Assignment Operators
+let x = 10;
+x+= 5;
+//Output: 15
+
+//Comparison Operators
+const a = 3;
+   //greater than operator
+    console.log(a>2);
+    //Output : true
+
+    //Not Equal to operator
+    const b = 4;
+    console.log(a!= 2);
+    //Output: true
+
+//Arithmetic Operators
+ let x = 5;
+ let y = 2;
+
+ console.log(a+b);
+ console.log(a-b);
+ console.log(a*b);
+ console.log(a/b);
+ console.log(a%b);
+ console.log(Math.pow(x,2));
+
+ //Output: 
+ //7
+ //3
+ //10
+ //2.5
+ //1
+ //25
+
+ //Logical Operator
+ let z = 4;
+ console.log((c>2)&&(c<2));
+ //Output: false
+
+ //String Operator
+ let str1 = "Shree";
+ let str2 = "Ram";
+ console.log(str1+str2);
+ //Output: Shree Ram
+
+ //Conditional (ternary) operator
+ let age = 18;
+ let condition = (age>=20) ? "allow": "notAllow";
+ console.log(condition);
+ //Output: notAllow
+
+//Comma operator
+let p = 1;
+p = (p++,p);
+console.log(p);
+ //Output: 2
+
+ //Relational operator 
+ console.log(3>4);
+ //Output: false
